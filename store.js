f4869c179015a390e688b0784e0d6c57407aa7e7
iliatcymbal <iliatcymbal@gmail.com>
2017-12-17 14:58:07 +0200

    Added redux dev tool

--- store.js
+++ store.js
@@ -4,19 +4,20 @@ import { createStore } from 'redux';
 // action should be an object with next pattern:
 // { type: 'ACTION_NAME', payload?: any_js_value }
 const date = (state = new Date().getFullYear(), action) => {
-    switch (action.type) {
-        case 'INCREASE_YEAR': {
-            return state + 1;
-        }
-
-        case 'DECREASE_YEAR':
-            return state - 1;
+  switch (action.type) {
+    case 'INCREASE_YEAR': {
+      return state + 1;
     }
 
-    return state;
+    case 'DECREASE_YEAR':
+      return state - 1;
+  }
+
+  return state;
 };
 
-export const store = createStore(date);
-// store.subscribe(function () {}) ==> function invokes every time when state was changed
-// store.dispatch(action) ==> 'says' to store to update state
-// store.getState() ==> returns actual state
+export const store = createStore(
+  date,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+// second argument is used to add redux dev tool (should be pre-installed in browser)
