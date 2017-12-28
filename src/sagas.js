d30193fa01a0f60afa7fcd8cc6204b46693bca40
iliatcymbal <iliatcymbal@gmail.com>
2017-12-29 01:14:02 +0200

    Added comments

--- src/sagas.js
+++ src/sagas.js
@@ -1,3 +1,3 @@
 export function* testSaga() {
-  console.log('Hello Sagas!')
+  console.log('Hello Sagas!');
 }
--- src/store.js
+++ src/store.js
@@ -3,14 +3,17 @@ import createSagaMiddleware from 'redux-saga';
 import * as reducers from './reducers';
 import { testSaga } from './sagas'
 
+// create saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
 const rootReducers = combineReducers(reducers);
 
 export const store = createStore(
   rootReducers,
+  // integrate middleware to store
   applyMiddleware(sagaMiddleware)
   //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+// run sagas
 sagaMiddleware.run(testSaga);
